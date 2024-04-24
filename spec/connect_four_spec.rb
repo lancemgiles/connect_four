305dc25815a88920997bde94288c3c92f95df756
lance <dogmandevices@gmail.com>
2024-04-23 22:32:49 -0500

    create test for win conditions, but this approach does not seem to work at all. should programmatically define win conditions instead of hard coding coordinates

--- spec/connect_four_spec.rb
+++ spec/connect_four_spec.rb
@@ -9,4 +9,23 @@ describe ConnectFour do
       expect(board.build).to eql([[0, 1, 2, 3], [0, 1, 2, 3], [0, 1, 2, 3], [0, 1, 2, 3]])
     end
   end
+  describe 'WIN' do
+    it 'is an array of possible winning positions' do
+      game = ConnectFour.new
+      expect(game.WIN).to eql(
+        [
+          [@board[0][0], @board[0][1], @board[0][2], @board[0][3]],
+          [@board[1][0], @board[1][1], @board[1][2], @board[1][3]],
+          [@board[2][0], @board[2][1], @board[2][2], @board[2][3]],
+          [@board[3][0], @board[3][1], @board[3][2], @board[3][3]],
+          [@board[0][0], @board[1][0], @board[2][0], @board[3][0]],
+          [@board[0][1], @board[1][1], @board[2][1], @board[3][1]],
+          [@board[0][2], @board[1][2], @board[2][2], @board[3][2]],
+          [@board[0][3], @board[1][3], @board[2][3], @board[3][3]],
+          [@board[0][0], @board[1][1], @board[2][2], @board[3][3]],
+          [@board[0][3], @board[1][2], @board[2][1], @board[3][0]]
+        ]
+      )
+    end
+  end
 end
