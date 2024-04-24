9cba990936a129a9ab5fc2f7b465e5cf5b81a7c3
lance <dogmandevices@gmail.com>
2024-04-23 23:45:42 -0500

    rework board so each cell holds the value to be printed instead of its index

--- lib/connect_four.rb
+++ lib/connect_four.rb
@@ -4,25 +4,23 @@
 class ConnectFour
   attr_accessor :board
 
-  WIN = [
-    [@board[0][0], @board[0][1], @board[0][2], @board[0][3]],
-    [@board[1][0], @board[1][1], @board[1][2], @board[1][3]],
-    [@board[2][0], @board[2][1], @board[2][2], @board[2][3]],
-    [@board[3][0], @board[3][1], @board[3][2], @board[3][3]],
-    [@board[0][0], @board[1][0], @board[2][0], @board[3][0]],
-    [@board[0][1], @board[1][1], @board[2][1], @board[3][1]],
-    [@board[0][2], @board[1][2], @board[2][2], @board[3][2]],
-    [@board[0][3], @board[1][3], @board[2][3], @board[3][3]],
-    [@board[0][0], @board[1][1], @board[2][2], @board[3][3]],
-    [@board[0][3], @board[1][2], @board[2][1], @board[3][0]]
-  ].freeze
-
-  def initialize
-    @board = []
-    build
-  end
-
   def build
-    @board = Array.new(4) { Array.new(4) { |n| n } }
+    @board = Array.new(4) { Array.new(4) { |n| n = '_' } }
+  end
+end
+
+# general player class for human or computer players
+class Player < ConnectFour
+  attr_accessor :history
+  def make_move
+    puts 'Select a column to make your move. (0~3)'
+    ans = gets.chomp.to_i
+    until ans <= 3 && ans >= 0
+      puts 'Enter a valid column.'
+      puts 'It must be a number between 0 and 3.'
+      puts 'It cannot already be occupied.'
+      ans = gets.chomp.to_i
+    end
+    ans
   end
 end
--- spec/connect_four_spec.rb
+++ spec/connect_four_spec.rb
@@ -4,28 +4,18 @@ require './lib/connect_four'
 
 describe ConnectFour do
   describe '#build' do
-    it 'builds a 16x16 grid and returns the array' do
+    it 'builds a 4x4 grid and returns the array' do
       board = ConnectFour.new
-      expect(board.build).to eql([[0, 1, 2, 3], [0, 1, 2, 3], [0, 1, 2, 3], [0, 1, 2, 3]])
-    end
-  end
-  describe 'WIN' do
-    it 'is an array of possible winning positions' do
-      game = ConnectFour.new
-      expect(game.WIN).to eql(
-        [
-          [@board[0][0], @board[0][1], @board[0][2], @board[0][3]],
-          [@board[1][0], @board[1][1], @board[1][2], @board[1][3]],
-          [@board[2][0], @board[2][1], @board[2][2], @board[2][3]],
-          [@board[3][0], @board[3][1], @board[3][2], @board[3][3]],
-          [@board[0][0], @board[1][0], @board[2][0], @board[3][0]],
-          [@board[0][1], @board[1][1], @board[2][1], @board[3][1]],
-          [@board[0][2], @board[1][2], @board[2][2], @board[3][2]],
-          [@board[0][3], @board[1][3], @board[2][3], @board[3][3]],
-          [@board[0][0], @board[1][1], @board[2][2], @board[3][3]],
-          [@board[0][3], @board[1][2], @board[2][1], @board[3][0]]
-        ]
-      )
+      expect(board.build).to eql([%w[_ _ _ _], %w[_ _ _ _], %w[_ _ _ _], %w[_ _ _ _]])
     end
   end
 end
+
+# describe Player do
+#   describe '#make_move' do
+#     it 'returns coordinates inputted by a player' do
+#       player = Player.new
+#       expect(player.make_move).to eql([0, 0])
+#     end
+#   end
+# end
