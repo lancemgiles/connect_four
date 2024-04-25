1c19b09c43df98e7e339c0666f5c14d466986e30
lance <dogmandevices@gmail.com>
2024-04-24 20:13:48 -0500

    simplified #drop_piece by using #between to limit column input and an if statement to determine if there is an empty cell

--- lib/connect_four.rb
+++ lib/connect_four.rb
@@ -8,12 +8,8 @@ class ConnectFour
     puts "Let's play Connect Four!"
     puts 'The goal is to connect four of your symbols together.'
     puts 'You can only choose a column! Your piece will go to the lowest position.'
-    @board = build
+    @board = Array.new(4) { Array.new(4) { '_' } }
     show_board
-  end
-
-  def build
-    Array.new(4) { Array.new(4) { '_' } }
   end
 
   def show_board
@@ -22,43 +18,31 @@ class ConnectFour
     end
     puts ' 0 1 2 3'
   end
-
-  def not_filled?(col)
-    @board[0..3][col].any? { |c| c != '_' }
-    p @board[0..3][col]
-  end
 end
 
 # general player class for human or computer players
 class Player < ConnectFour
-  attr_accessor :history
-
   def choose_col
-    loop do
-      puts 'Select a column to make your move. (0~3)'
+    puts 'Select a column to make your move. (0~3)'
+    ans = gets.chomp.to_i
+    until ans <= 3 && ans >= 0
+      puts 'Enter a valid column.'
+      puts 'It must be a number between 0 and 3.'
       ans = gets.chomp.to_i
-      until ans <= 3 && ans >= 0
-        puts 'Enter a valid column.'
-        puts 'It must be a number between 0 and 3.'
-        ans = gets.chomp.to_i
+    end
+    ans
+  end
+
+  def drop_piece(column, piece)
+    return false unless column.between?(0, 3)
+
+    @board[column].each_with_index do |cell, index|
+      if cell == '_'
+        @board[column][index] = piece
+        true
       end
-      return ans unless not_filled?(ans)
-
-      puts 'You must select an empty column'
     end
-  end
-
-  def find_empty_row(col)
-    @board.to_enum.with_index.reverse_each do |row, index|
-      return index if row[col] == '_'
-    end
-    nil
-  end
-
-  def drop_piece(piece)
-    col = choose_col
-    row = find_empty_row(col)
-    @board[row][col] = piece
+    false
   end
 end
 
